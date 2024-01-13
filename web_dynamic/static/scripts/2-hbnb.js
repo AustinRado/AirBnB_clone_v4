d202a111e7a9c747112d2c8623285d5ac6d45ddd
BENONI ESCKINDER <you@example.com>
2024-01-13 14:15:44 +0300

    Adding cache_id to links + moving in more static files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,15 +1,24 @@
 $(function () {
-        $.ajax({
-                url=" http://0.0.0.0:5001/api/v1/status/",
-                method: 'GET',
-                dataType: 'json',
-                success: function (result) {
-                        if result.status === "OK" {
-                                $('#api_status').addClass('available');
-                        }
-			else {
-				$('#api_status').removeClass('available');
-			}
-                }
-        });
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+  });
+
+  $.ajax({
+    url: ' http://0.0.0.0:5001/api/v1/status/',
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
 });
